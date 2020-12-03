52078a9fd5d2365f851c07ee598321ccfaa5650b
ama1y <632930757@qq.com>
2020-12-03 23:14:45 +0800

    deploy

---
+++ precache-manifest.33e920a07b9d501bc987a759e9da5330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87d6bba086acfc4cb0e",
+    "url": "/Weight-management-website-1/css/app.788f29d7.css"
+  },
+  {
+    "revision": "f6fe93127c373ae616437299c1e56d28",
+    "url": "/Weight-management-website-1/index.html"
+  },
+  {
+    "revision": "f87d6bba086acfc4cb0e",
+    "url": "/Weight-management-website-1/js/app.55be330e.js"
+  },
+  {
+    "revision": "2a2196ec0b6944c8a427",
+    "url": "/Weight-management-website-1/js/chunk-vendors.9445aee8.js"
+  },
+  {
+    "revision": "67aa99dacc24a7cdc72e13eb46259322",
+    "url": "/Weight-management-website-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Weight-management-website-1/robots.txt"
+  }
+]);
